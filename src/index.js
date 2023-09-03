fadf665ceacf2e700e03927bf54c64e3074f5e00
jojo sexton <jojolsexton@gmail.com>
2023-09-03 20:28:50 +1200

    try deploy again

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ReactDom from 'react-dom'
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
+<BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
 </BrowserRouter>
 );
